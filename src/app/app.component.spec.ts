b8365539fede140dad3fb32ab5af6ec22d920c94
Julius Skrisa <julius.skrisa@moduscreate.com>
2024-07-09 16:26:31 +0200

    deploy #1

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-todo-zadanie-julsk' title`, () => {
+  it(`should have the 'angular-todo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-todo-zadanie-julsk');
+    expect(app.title).toEqual('angular-todo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-todo-zadanie-julsk');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-todo');
   });
 });
