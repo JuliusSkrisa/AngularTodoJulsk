ebba1ccbb8552bfadcade6530ee332e47f4a8e42
Julius Skrisa <julius.skrisa@moduscreate.com>
2024-07-09 17:12:55 +0200

    Auto-generated commit

--- src/app/store/auth/auth.actions.ts
+++
@@ -1,3 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-
-export const login = createAction('[Auth] login', props<{ success: true }>());
--- src/app/store/todo/todo.actions.ts
+++
@@ -1,4 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { Todo } from '../../models/todo';
-
-export const addTodoItem = createAction('[User] Add Todo Item', props<{ todo: Todo }>());
